e4be3da5102f62dd73306c42647c5246f9eb94ec
Katie Wang <katie.wang1950f@gmail.com>
2020-06-08 17:16:56 -0400

    Complete Iteration 0

---
+++ ToDo App 2/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDo App 2
+//
+//  Created by Quan Wang on 6/8/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
