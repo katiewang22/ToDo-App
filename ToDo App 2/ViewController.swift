2df4530d66cf2065128f350b3f54a652d7d2bd6f
sugataekookie <57153170+sugataekookie@users.noreply.github.com>
2020-06-08 16:27:43 -0400

    Initial Commit

---
+++ ToDo App 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo App 2
+//
+//  Created by Quan Wang on 6/8/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDo App 2Tests/ToDo_App_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ToDo_App_2Tests.swift
+//  ToDo App 2Tests
+//
+//  Created by Quan Wang on 6/8/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo_App_2
+
+class ToDo_App_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
