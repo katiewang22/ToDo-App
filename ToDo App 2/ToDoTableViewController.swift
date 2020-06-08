9d98f7649ed3b28cdfa2ab492b3b67b0b006c4de
Katie Wang <katie.wang1950f@gmail.com>
2020-06-08 18:55:41 -0400

    Complete Iteration 2

--- ToDo App 2/ToDoTableViewController.swift
+++ ToDo App 2/ToDoTableViewController.swift
@@ -23,11 +23,25 @@ class ToDoTableViewController: UITableViewController {
         return [swift, dog]
     }
     
-    var toDos : [ToDo] = []
+    func getToDos () {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                    toDos = coreDataToDos
+                    tableView.reloadData()
+            }
+        }
+    }
+    
+    //var toDos : [ToDo] = []
+    var toDos : [ToDoCD] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        toDos = createToDos()
+        getToDos()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      getToDos()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,13 +52,15 @@ class ToDoTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         let toDo = toDos[indexPath.row]
-        
-        if toDo.important {
-          cell.textLabel?.text = "❗️" + toDo.name
-        } else {
-          cell.textLabel?.text = toDo.name
+
+        if let name = toDo.name {
+          if toDo.important {
+              cell.textLabel?.text = "❗️" + name
+          } else {
+              cell.textLabel?.text = toDo.name
+          }
         }
-        
+
         return cell
     }
     
@@ -65,7 +81,7 @@ class ToDoTableViewController: UITableViewController {
         }
         
         if let completeVC = segue.destination as? CompleteToDoViewController {
-        if let toDo = sender as? ToDo {
+        if let toDo = sender as? ToDoCD {
           completeVC.selectedToDo = toDo
           completeVC.previousVC = self
             }
